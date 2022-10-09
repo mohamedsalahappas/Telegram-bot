d83eed178930b1ec0d7d27c2f9ce9c1e64f40cf0
mohamedsalahappas <115418554+mohamedsalahappas@users.noreply.github.com>
2022-10-09 18:45:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "1816198459:AAEG2c-RD1cTZCJqCW9YcTUf13oYIRJ8vz8"
-APP_URL = "https://sidrabot.herokuapp.com/" + BOT_TOKEN
+BOT_TOKEN = "5760557907:AAEJnk7u40_t5LQwp_8lwm3P82uSRj1rcQ4"
+APP_URL = "https://boykdappi.herokuapp.com/" + BOT_TOKEN
